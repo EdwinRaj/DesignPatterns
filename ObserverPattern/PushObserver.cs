2424ecd3027a666c4510b01364e82401a9ee1c1b
Edwin Raj <reachedwinraj@gmail.com>
2014-07-02 23:33:00 +0100

    Observer - push model initial checkin

---
+++ ObserverPattern/PushObserver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+
+    public interface ICustomObserver<in T>
+    {
+        void Update(T updates);
+    }
+
+    public interface ICustomObservable<T>
+    {
+        void AddObserver(ICustomObserver<T> observer);
+        void RemoveObserver(ICustomObserver<T> observer);
+        void NotifyObserver();
+        void Update(T updates);
+    }
+
+    public class WeatherUpdates
+    {
+        public decimal Temperature { get; set; }
+        public decimal Humidity { get; set; }  
+    }
+
+    /// <summary>
+    /// This class is also knownas the subject
+    /// </summary>
+    public class PushTemperateObservable:ICustomObservable<WeatherUpdates>
+    {
+        readonly List<ICustomObserver<WeatherUpdates>> _observerCollection = new List<ICustomObserver<WeatherUpdates>>();
+        private WeatherUpdates _weatherUpdates;
+
+        public void AddObserver(ICustomObserver<WeatherUpdates> observer)
+        {
+            _observerCollection.Add(observer);    
+        }
+
+        public void RemoveObserver(ICustomObserver<WeatherUpdates> observer)
+        {
+            _observerCollection.Remove(observer);
+        }
+
+        
+        public void NotifyObserver()
+        {
+            _observerCollection.ForEach(x=>x.Update(_weatherUpdates));
+        }
+
+        public void Update(WeatherUpdates updates)
+        {
+            _weatherUpdates = updates;
+            NotifyObserver();
+        }
+    }
+
+    public class MobilePhoneTemperaturePushObserver : ICustomObserver<WeatherUpdates>
+    {
+        public void Update(WeatherUpdates updates)
+        {
+            Console.WriteLine("I am mobile Phone observer. Current Temperature {0} and Humidity {1}",updates.Temperature,updates.Temperature);
+        }
+    }
+
+    public class BillBoardTemperaturePushObserver : ICustomObserver<WeatherUpdates>
+    {
+        public void Update(WeatherUpdates updates)
+        {
+            Console.WriteLine("I am Bill Board observer. Current Temperature {0} and Humidity {1}", updates.Temperature, updates.Temperature);
+        }
+    }
+}
---
+++ ObserverPatternTest/PushObserverTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObserverPattern;
+
+namespace ObserverPatternTest
+{
+    [TestClass]
+    public class PushObserverTests
+    {
+        [TestMethod]
+        public void PushBasedObserverTest()
+        {
+            ICustomObservable<WeatherUpdates> observable = new PushTemperateObservable();
+            ICustomObserver<WeatherUpdates> mobileObserver = new MobilePhoneTemperaturePushObserver();
+            ICustomObserver<WeatherUpdates> billBoardObserver = new BillBoardTemperaturePushObserver();
+            observable.AddObserver(mobileObserver);
+            observable.AddObserver(billBoardObserver);
+
+            observable.Update(new WeatherUpdates {Humidity = 100, Temperature = 35});
+            observable.Update(new WeatherUpdates { Humidity = 90, Temperature = 30 });
+        }
+    }
+}
